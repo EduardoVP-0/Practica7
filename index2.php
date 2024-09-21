cc5458ab587cf6adf36f5091d0ac9be5c4ff592b
Eduardo <vplalo11@gmail.com>
2024-09-20 22:18:34 -0600

    VERSION1

---
+++ index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hola HTML</p>
+    
+    <?php
+    echo 'hola PHP';
+    echo '<h1>hola php</h1>';
+    echo '<hr>';
+    echo '<br>';
+    
+    //comentario
+    $miNombre= 'Eduardo'; //VARIABLES
+    $miDireccion='And 29 Casa 14 Manzana 5 Inf Grijalva';
+    
+    echo '<img src="IMG/PERFIL EDUARDO.jpg">';
+    echo '<br>';
+    echo 'Mi nombre es:'.$miNombre;
+    echo '<br>';
+    echo 'Mi dirección es:'.$miDireccion;
+    //AGREGAR DIRECCION, TELEFONO E IMAGEN FOTO
+    ?>
+</body>
+</html>
